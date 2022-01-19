5d3764271c5c11d6188a715d38e658595bb9835f
Kirillov Evgeny <kelkrsru@gmail.com>
2022-01-19 16:31:20 +0700

    Добавлены urls и view-функции в приложении posts

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse(status=200, content='Main page')
+
+
+def group_posts(request, slug):
+    return HttpResponse(status=200, content=f"It's group for {slug}")

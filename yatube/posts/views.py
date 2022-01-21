c19d9c5ea77ad4961ec5644116e718743f1caa20
Kirillov Evgeny <kelkrsru@gmail.com>
2022-01-21 14:57:39 +0700

    Добавлены шаблоны и статические файлы

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,8 +4,22 @@ from django.http.response import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse(status=200, content='Main page')
+    template: str = 'posts/index.html'
+    title = 'Главная страница'
+    header = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+        'header': header,
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(status=200, content=f"It's group for {slug}")
+    template: str = 'posts/group_list.html'
+    title = 'Страница записи'
+    header = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title': title,
+        'header': header,
+    }
+    return render(request, template, context)
